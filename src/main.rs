47e9944bc2ed1c7c50c44cf3741916d82e90bb6c
GhtGhoster <32255934+GhtGhoster@users.noreply.github.com>
2023-03-03 12:48:28 +0100

    Add egui

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,19 @@ use macroquad::prelude::*;
 #[macroquad::main("mq-wasm-pages")]
 async fn main() {
     loop {
+        // ui
+        egui_macroquad::ui(|egui_ctx| {
+            egui::Window::new("top")
+                .show(egui_ctx, |ui| {
+                    ui.label("kek");
+                }
+            );
+        });
+
+        // rendering
         clear_background(BLACK);
 
+        egui_macroquad::draw();
         next_frame().await
     }
 }
